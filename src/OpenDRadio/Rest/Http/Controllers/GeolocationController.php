2c93f3ecb339cc3ffcbb93766e5707cc2e6d6577
Matej Kieres <e@mae.to>
2015-03-27 04:30:21 +0100

    Cache the geolocation results

--- src/OpenDRadio/Rest/Http/Controllers/GeolocationController.php
+++ src/OpenDRadio/Rest/Http/Controllers/GeolocationController.php
@@ -1,11 +1,20 @@
 <?php namespace OpenDRadio\Rest\Http\Controllers;
 
+use Cache;
 use Carbon;
 use Request;
 use Response;
 use MaxMind\Db\Reader;
 
 class GeolocationController extends BaseController {
+
+        /**
+         * The query results cache duration.
+         *
+         * @var \Number
+         * @access protected
+         */
+        protected $cacheDuration = 30;
 
 	/**
 	 * Geo clue the location of the request ip address.
@@ -17,10 +26,25 @@ class GeolocationController extends BaseController {
 		// Get the ip address
 		$ip = Request::getIp();
 
-		// Find a geoip record
-		$reader = new Reader(ROOT_PATH.'/provider/GeoIP/GeoLite2-City.mmdb');
-		$record = $reader->get($ip);
-		$reader->close();
+		// Create a cache key based on the IP type
+		if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4))
+		{
+			$cacheKey = sprintf('geolocation:ipv4_%s', str_replace('.', '_', $ip));
+		}
+		else
+		{
+			$cacheKey = sprintf('geolocation:ipv6_%s', str_replace(':', '_', strtolower($ip)));
+		}
+
+		$record = Cache::remember($cacheKey, $this->cacheDuration, function() use ($ip)
+		{
+			// Find a geoip record
+			$reader = new Reader(ROOT_PATH.'/provider/GeoIP/GeoLite2-City.mmdb');
+			$record = $reader->get($ip);
+			$reader->close();
+			
+			return $record;
+		});
 
 		if(empty($record))
 		{
