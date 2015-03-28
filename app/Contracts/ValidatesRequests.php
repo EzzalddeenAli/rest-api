997aaa4643dee807e3380fc4a979c7ce6908ea68
Matej Kieres <e@mae.to>
2015-03-28 01:57:35 +0100

    Add a new contract - The ValidatesRequests contract is responsible to validate incoming requests using the Laravel Validator

---
+++ app/Contracts/ValidatesRequests.php
@@ -0,0 +1,49 @@
+<?php namespace App\Contracts;
+
+use Response;
+use Validator;
+
+trait ValidatesRequests {
+
+	/**
+	 * Validate the given request with the given rules.
+	 *
+	 * @param  array  $data
+	 * @param  array  $rules
+	 * @param  array  $messages
+	 * @return void
+	 */
+	public function validate(array $data, array $rules, array $messages = array())
+	{
+		$validator = Validator::make($data, $rules, $messages);
+
+		if ($validator->fails())
+		{
+			$this->throwValidationException($validator);
+		}
+	}
+
+	/**
+	 * Throw the failed validation exception.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Illuminate\Contracts\Validation\Validator  $validator
+	 * @return void
+	 */
+	protected function throwValidationException($validator)
+	{
+		Response::send(400, null, $this->formatValidationErrors($validator));
+	}
+
+	/**
+	 * Format the validation errors to be returned.
+	 *
+	 * @param  \Illuminate\Validation\Validator  $validator
+	 * @return array
+	 */
+	protected function formatValidationErrors($validator)
+	{
+		return $validator->errors()->getMessages();
+	}
+
+}
